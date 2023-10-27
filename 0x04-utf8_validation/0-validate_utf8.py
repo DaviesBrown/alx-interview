48366866155c098f813d33159dcbba6ab2392cbf
Davies Brown <nwosudavid77@gmail.com>
2023-10-27 20:46:17 +0100

    utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,15 +10,31 @@ def validUTF8(data: List[int]) -> bool:
     """
     determines if a given dataset is valid UTF-8 encoding
     """
-    succ10 = 0
-    for i in data:
-        b = bin(i).replace('0b', '').rjust(8, '0')
-        if len(b) > 8:
-            b = b[1:]
-        if succ10 != 0:
-            succ10 -= 1
-            if not b.startswith('10'):
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    byte_cnt = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for symbol in data:
+        bit = 1 << 7
+        if byte_cnt == 0:
+            while (bit & symbol):
+                byte_cnt += 1
+                bit = bit >> 1
+
+            if byte_cnt == 0:
+                continue
+            if byte_cnt == 1 or byte_cnt > 4:
                 return False
-        elif b[0] == '1':
-            succ10 = len(b.split('0')[0]) - 1
-    return True
+        else:
+
+            if not (symbol & bit1 and not (symbol & bit2)):
+                return False
+        byte_cnt -= 1
+
+    if byte_cnt:
+        return False
+    else:
+        return True
