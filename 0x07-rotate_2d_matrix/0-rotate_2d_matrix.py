f615abff89a2cbac8e1a5b8dc0e7124cb672e518
Davies Brown <nwosudavid77@gmail.com>
2023-11-13 13:45:53 +0100

    rotate 2d matrix 90deg

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+rotate 2d matrix
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List]):
+    """ rotate 2d matrix"""
+    new_matrix = [tuple(i) for i in matrix]
+    mat_len = len(matrix)
+    length = mat_len - 1
+    for i in range(mat_len):
+        print(length)
+        for j in range(len(matrix[i])):
+            matrix[i][j] = new_matrix[length][i]
+            length -= 1
+        length = mat_len - 1
