0fa5c7ab59b9a98f977969c12f47d9b72b396af0
Davies Brown <nwosudavid77@gmail.com>
2023-11-15 11:52:50 +0100

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,6 @@ def rotate_2d_matrix(matrix):
     mat_len = len(matrix)
     length = mat_len - 1
     for i in range(mat_len):
-        print(length)
         for j in range(len(matrix[i])):
             matrix[i][j] = new_matrix[length][i]
             length -= 1
